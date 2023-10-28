37c403b9b2cbc627de3e4d08e6694b584e889bae
Alfredo <kmstry214@gmail.com>
2023-10-27 19:33:34 -0500

    add logo to navbar file

--- dj-website/src/components/Navbar/Navbar.jsx
+++ dj-website/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
+import Kmstry from '../../assets/kmstry.svg'
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center py-4 px-6 ">
-      <h1 className="text-2xl font-pacifico">KMSTRY</h1>
-      <ul className="flex flex-row  gap-10 text-lg  ">
+    <nav className="flex flex-row justify-between items-center  px-6 py-2 ">
+      <img src={Kmstry} alt="logo" />
+      {/* <h1 className="text-2xl font-pacifico">KMSTRY</h1> */}
+      <ul className="flex flex-row  gap-10 text-md  ">
         <li>Home</li>
         <li>About</li>
         <li>Mixes</li>
