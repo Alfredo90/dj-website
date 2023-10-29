9ea13dcc5153aee614d306db96ab7b140630ffc9
Alfredo <kmstry214@gmail.com>
2023-10-28 23:03:19 -0500

    embeded React player for soundcloud

--- dj-website/src/components/About/About.jsx
+++ dj-website/src/components/About/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className=" flex flex-col justify-center items-center h-screen w-screen border-2 gap-10">
+    <section className=" flex flex-col justify-center items-center h-screen w-screen  gap-10 ">
       <h1 className="text-5xl">About Me</h1>
-      <p className="line-clamp-4 border-2 text-left w-2/4 h-2/4 text-2xl flex">
+      <p className="text-justify w-2/4 h-2/4 text-2xl leading-relaxed indent-8  ">
         "As a dedicated disc jockey, I've had the privilege of spinning tunes
         for a decade now, and the journey has been nothing short of
         exhilarating. Over the years, I've fine-tuned my craft, blending genres
@@ -16,7 +16,7 @@ const About = () => {
         committed to making your special moments even more memorable. I'm
         excited to continue this musical journey with you, so don't hesitate to
         reach out. Let's make magic together on the dance floor, creating
-        memories that will last a lifetime.""
+        memories that will last a lifetime."
       </p>
     </section>
   );
--- dj-website/src/components/Mixes/Mixes.jsx
+++ dj-website/src/components/Mixes/Mixes.jsx
@@ -1,7 +1,18 @@
+import Kmstry from "../../assets/kmstry.svg";
 import React from "react";
+import ReactPlayer from "react-player";
 
 const Mixes = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="flex flex-row justify-center items-center h-screen w-screen ">
+      <ReactPlayer
+        width="600px"
+        // playIcon
+        // light={<img src={Kmstry} />}
+        url="https://soundcloud.com/user-236976425/tracks"
+      />
+    </div>
+  );
 };
 
 export default Mixes;
