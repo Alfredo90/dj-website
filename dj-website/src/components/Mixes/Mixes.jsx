27312d267177b7e79a37bb5b79724f5ec6bff564
Alfredo <kmstry214@gmail.com>
2023-10-28 23:07:36 -0500

    test

--- dj-website/src/components/Mixes/Mixes.jsx
+++ dj-website/src/components/Mixes/Mixes.jsx
@@ -5,12 +5,15 @@ import ReactPlayer from "react-player";
 const Mixes = () => {
   return (
     <div className="flex flex-row justify-center items-center h-screen w-screen ">
-      <ReactPlayer
-        width="600px"
-        // playIcon
-        // light={<img src={Kmstry} />}
-        url="https://soundcloud.com/user-236976425/tracks"
-      />
+      <div className="border-2 rounded-lg shadow-lg shadow-[#54ECC4]">
+        <ReactPlayer
+          width="640px"
+          height="640px"
+          // playIcon
+          // light={<img src={Kmstry} />}
+          url="https://soundcloud.com/user-236976425/tracks"
+        />
+      </div>
     </div>
   );
 };
